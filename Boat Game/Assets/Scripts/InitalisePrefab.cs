5010563fb1419c4deb71f20b8c757c53f9b8f743
egomm <egomyt@gmail.com>
2023-04-15 22:27:51 +1200

    Working on changing wave height/speed

--- Boat Game/Assets/Scripts/InitalisePrefab.cs
+++ Boat Game/Assets/Scripts/InitalisePrefab.cs
@@ -12,14 +12,26 @@ public class InitalisePrefab : MonoBehaviour
     {
         for (int x = -5; x < 5; x++) {
             for (int z = -5; z < 5; z++) {
-                Instantiate(water, new Vector3(10*x, 0, 10*z), Quaternion.identity);
+                GameObject spawned = Instantiate(water, new Vector3(10*x, 0, 10*z), Quaternion.identity);
             }
+        }
+        Invoke("ManagePrefabs", 0.2f);
+    }
+    
+    void ManagePrefabs() {
+        GameObject component = GameObject.Find("Ocean(Clone)");
+        component.GetComponent<WaveManager>().amplitude += 0.02f;
+        if (component.GetComponent<WaveManager>().amplitude <= 1) { 
+            Invoke("ManagePrefabs", 0.2f);
+        } else {
+            Debug.Log("Done");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //GameObject component = GameObject.Find("Ocean(Clone)");
+        //component.GetComponent<WaveManager>().amplitude += Time.deltaTime/10;
     }
 }
